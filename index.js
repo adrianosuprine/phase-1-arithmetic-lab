977530e35a805b567f9d47250677ad07a852c285
adrianosuprine <adrianosuprine@gmail.com>
2023-12-12 21:43:40 +0300

    compute js arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const[num1,num2,num3,num4]=[2,31,19,5];
+const arr = [1,10,11,9,18,20,12,2];
+let multiply =num1*num2;
+let random = Math.floor(Math.random()*10)+1;
+let mod = num3%num4;
+let max = Math.max(...arr);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(multiply);
+console.log(random);
+console.log(mod);
+console.log(max);
